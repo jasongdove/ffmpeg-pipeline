58e1193d9790e1ded5f7b4cc77813bb7913478ce
Jason Dove <jason@jasondove.me>
2023-02-14 22:55:38 -0600

    fix: export AudioInputFile

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { VideoInputFile } from "./inputFile";
 import { PipelineStep } from "./interfaces/pipelineStep";
 
 export { SoftwarePipelineBuilder } from "./pipeline/softwarePipelineBuilder";
-export { VideoInputFile } from "./inputFile";
+export { AudioInputFile, VideoInputFile } from "./inputFile";
 export { VideoStream } from "./mediaStream";
 export { FFmpegState } from "./ffmpegState";
 export { FrameState } from "./frameState";
